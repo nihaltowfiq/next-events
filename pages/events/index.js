748ff14809bd1d06a8619060245aaa11b45e95a3
nihaltowfiq <nihaltowfiq@gmail.com>
2021-09-20 00:20:37 +0600

    added SSG

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,22 +1,22 @@
-import { Fragment } from 'react';
 import { useRouter } from 'next/router';
-import { getAllEvents } from '../../dummy-data';
+import { Fragment } from 'react';
 import EventList from '../../components/Events/EventList';
 import EventSearch from '../../components/Events/EventSearch';
+import { getAllEvents } from '../../dummy-data';
 
 const AllEvents = () => {
-    const router = useRouter();
-    const allEvents = getAllEvents();
-    const findEventHandler = (month, year) => {
-        router.push(`/events/${month}/${year}`);
-    };
+	const router = useRouter();
+	const allEvents = getAllEvents();
+	const findEventHandler = (month, year) => {
+		router.push(`/events/${month}/${year}`);
+	};
 
-    return (
-        <Fragment>
-            <EventSearch onSearch={findEventHandler} />
-            <EventList items={allEvents} />
-        </Fragment>
-    );
+	return (
+		<Fragment>
+			<EventSearch onSearch={findEventHandler} />
+			<EventList items={allEvents} />
+		</Fragment>
+	);
 };
 
 export default AllEvents;

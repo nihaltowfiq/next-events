f5d173f567bace808b6c281506bee82169b9f0b9
nihaltowfiq <nihaltowfiq@gmail.com>
2021-11-21 00:10:00 +0600

    events page validation added

--- components/Events/EventList.js
+++ components/Events/EventList.js
@@ -2,13 +2,15 @@ import EventItem from './EventItem';
 import classes from './EventList.module.css';
 
 const EventList = ({ items }) => {
-    return (
-        <ul className={classes.list}>
-            {items.map((event) => (
-                <EventItem key={event.id} {...event} />
-            ))}
-        </ul>
-    );
+	return (
+		<ul className={classes.list}>
+			{items?.length > 0 ? (
+				items.map((event) => <EventItem key={event.id} {...event} />)
+			) : (
+				<p style={{ textAlign: 'center' }}>No event found!</p>
+			)}
+		</ul>
+	);
 };
 
 export default EventList;

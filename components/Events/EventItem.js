187597eb937120e18bfb3cd2db95be5f536be066
nihaltowfiq <nihaltowfiq@gmail.com>
2022-01-15 02:05:44 +0600

    refactored

--- components/Events/EventItem.js
+++ components/Events/EventItem.js
@@ -1,44 +1,44 @@
-import classes from './EventItem.module.css';
-import Button from '../UI/Button';
-import DateIcon from '../Icons/DateIcon';
 import AddressIcon from '../Icons/AddressIcon';
 import ArrowRightIcon from '../Icons/ArrowRightIcon';
+import DateIcon from '../Icons/DateIcon';
+import Button from '../UI/Button';
+import classes from './EventItem.module.css';
 
 const EventItem = ({ id, title, image, location, date }) => {
-    const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
-        day: 'numeric',
-        month: 'long',
-        year: 'numeric',
-    });
+	const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
+		day: 'numeric',
+		month: 'long',
+		year: 'numeric',
+	});
 
-    const formattedLocation = location.replace(', ', '\n');
+	const formattedLocation = location.replace(', ', '\n');
 
-    return (
-        <li className={classes.item}>
-            <img src={`/${image}`} alt={title} />
-            <div className={classes.content}>
-                <div className={classes.summary}>
-                    <h2>{title}</h2>
-                    <div className={classes.date}>
-                        <DateIcon />
-                        <time>{humanReadableDate}</time>
-                    </div>
-                    <div className={classes.address}>
-                        <AddressIcon />
-                        <address>{formattedLocation}</address>
-                    </div>
-                </div>
-                <div className={classes.actions}>
-                    <Button link={`/events/${id}`}>
-                        <span>Explore Event</span>
-                        <span className={classes.icon}>
-                            <ArrowRightIcon />
-                        </span>
-                    </Button>
-                </div>
-            </div>
-        </li>
-    );
+	return (
+		<li className={classes.item}>
+			<img src={`/${image}`} alt={title} />
+			<div className={classes.content}>
+				<div className={classes.summary}>
+					<h2>{title}</h2>
+					<div className={classes.date}>
+						<DateIcon />
+						<time>{humanReadableDate}</time>
+					</div>
+					<div className={classes.address}>
+						<AddressIcon />
+						<address>{formattedLocation}</address>
+					</div>
+				</div>
+				<div className={classes.actions}>
+					<Button link={`/events/${id}`}>
+						<span>Explore Event</span>
+						<span className={classes.icon}>
+							<ArrowRightIcon />
+						</span>
+					</Button>
+				</div>
+			</div>
+		</li>
+	);
 };
 
 export default EventItem;
--- components/Events/EventSearch.js
+++ components/Events/EventSearch.js
@@ -1,50 +1,50 @@
-import classes from './EventSearch.module.css';
-import Button from '../UI/Button';
 import { useRef } from 'react';
+import Button from '../UI/Button';
+import classes from './EventSearch.module.css';
 
 const EventSearch = ({ onSearch }) => {
-    const inputYearRef = useRef();
-    const inputMonthRef = useRef();
+	const inputYearRef = useRef();
+	const inputMonthRef = useRef();
 
-    const submitHandler = (e) => {
-        e.preventDefault();
-        const selectedMonth = inputMonthRef.current.value;
-        const selectedYear = inputYearRef.current.value;
-        onSearch(selectedMonth, selectedYear);
-    };
-    return (
-        <form className={classes.form} onSubmit={submitHandler}>
-            <div className={classes.controls}>
-                <div className={classes.control}>
-                    <label htmlFor="year">Year</label>
-                    <select name="year" id="year" ref={inputYearRef}>
-                        <option value="2021">2021</option>
-                        <option value="2022">2022</option>
-                    </select>
-                </div>
+	const submitHandler = (e) => {
+		e.preventDefault();
+		const selectedMonth = inputMonthRef.current.value;
+		const selectedYear = inputYearRef.current.value;
+		onSearch(selectedMonth, selectedYear);
+	};
+	return (
+		<form className={classes.form} onSubmit={submitHandler}>
+			<div className={classes.controls}>
+				<div className={classes.control}>
+					<label htmlFor="year">Year</label>
+					<select name="year" id="year" ref={inputYearRef}>
+						<option value="2021">2021</option>
+						<option value="2022">2022</option>
+					</select>
+				</div>
 
-                <div className={classes.control}>
-                    <label htmlFor="month">Month</label>
-                    <select name="month" id="month" ref={inputMonthRef}>
-                        <option value="1">January</option>
-                        <option value="2">February</option>
-                        <option value="3">March</option>
-                        <option value="4">April</option>
-                        <option value="5">May</option>
-                        <option value="6">June</option>
-                        <option value="7">July</option>
-                        <option value="8">August</option>
-                        <option value="9">September</option>
-                        <option value="10">October</option>
-                        <option value="11">November</option>
-                        <option value="12">December</option>
-                    </select>
-                </div>
-            </div>
+				<div className={classes.control}>
+					<label htmlFor="month">Month</label>
+					<select name="month" id="month" ref={inputMonthRef}>
+						<option value="1">January</option>
+						<option value="2">February</option>
+						<option value="3">March</option>
+						<option value="4">April</option>
+						<option value="5">May</option>
+						<option value="6">June</option>
+						<option value="7">July</option>
+						<option value="8">August</option>
+						<option value="9">September</option>
+						<option value="10">October</option>
+						<option value="11">November</option>
+						<option value="12">December</option>
+					</select>
+				</div>
+			</div>
 
-            <Button>Find Events</Button>
-        </form>
-    );
+			<Button>Find Events</Button>
+		</form>
+	);
 };
 
 export default EventSearch;

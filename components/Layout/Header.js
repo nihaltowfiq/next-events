40f736bbf7a4e93bef6d7865e63e5e965b1558c3
nihaltowfiq <nihaltowfiq@gmail.com>
2022-01-15 02:06:50 +0600

    logo changed

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -2,21 +2,21 @@ import Link from 'next/link';
 import classes from './Header.module.css';
 
 const Header = () => {
-    return (
-        <header className={classes.header}>
-            <div className={classes.logo}>
-                <Link href="/">LOGO</Link>
-            </div>
+	return (
+		<header className={classes.header}>
+			<div className={classes.logo}>
+				<Link href="/">NextEvents</Link>
+			</div>
 
-            <nav className={classes.navigation}>
-                <ul>
-                    <li>
-                        <Link href="/events">Browse All Events</Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    );
+			<nav className={classes.navigation}>
+				<ul>
+					<li>
+						<Link href="/events">Browse All Events</Link>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
 };
 
 export default Header;
